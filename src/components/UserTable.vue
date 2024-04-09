b6cbe4cc7c13346dcd266bdaf8bbd794d4bf4aee
AnonymouseHucker29 <brent.baylon29@gmail.com>
2024-04-09 12:46:14 +0800

    refactor: moved table into a separate component

---
+++ src/components/UserTable.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps<{ filteredUsers: any }>()
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+</script>
+<template>
+  <Table id="table" class="w-full overflow-hidden">
+    <TableHeader>
+      <TableRow>
+        <TableHead class="font-bold text-center w-1/4">NAME</TableHead>
+        <TableHead class="font-bold text-center w-1/4">EMAIL</TableHead>
+        <TableHead class="font-bold text-center w-1/4">CITY</TableHead>
+        <TableHead class="font-bold text-center w-1/4">COMPANY</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <template v-for="user in filteredUsers" :key="user.id">
+        <TableRow
+          class="cursor-pointer"
+          @click="$router.push({ name: 'user', params: { id: user.id, username: user.name } })"
+        >
+          <TableCell class="text-center">{{ user.name }}</TableCell>
+          <TableCell class="text-center">{{ user.email }}</TableCell>
+          <TableCell class="text-center">{{ user.address.city }}</TableCell>
+          <TableCell class="text-center">{{ user.company.name }}</TableCell>
+        </TableRow>
+      </template>
+    </TableBody>
+  </Table>
+</template>
+<style scoped>
+#table::-webkit-scrollbar {
+  display: none;
+}
+
+#table {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from '@/components/ui/table'
 import { Input } from '@/components/ui/input'
+import UserTable from '@/components/UserTable.vue'
 import fetchUsers from '@/api/fetchUsers'
 import { ref, computed } from 'vue'
 import type { User } from '@/types/User'
@@ -41,39 +34,7 @@ fetchUsersAPI()
         class="w-[38rem]"
         placeholder="Search by a user's name"
       />
-      <Table id="table" class="w-full overflow-hidden">
-        <TableHeader>
-          <TableRow>
-            <TableHead class="font-bold text-center w-1/4">NAME</TableHead>
-            <TableHead class="font-bold text-center w-1/4">EMAIL</TableHead>
-            <TableHead class="font-bold text-center w-1/4">CITY</TableHead>
-            <TableHead class="font-bold text-center w-1/4">COMPANY</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <template v-for="user in filteredUsers" :key="user.id">
-            <TableRow
-              class="cursor-pointer"
-              @click="$router.push({ name: 'user', params: { id: user.id, username: user.name } })"
-            >
-              <TableCell class="text-center">{{ user.name }}</TableCell>
-              <TableCell class="text-center">{{ user.email }}</TableCell>
-              <TableCell class="text-center">{{ user.address.city }}</TableCell>
-              <TableCell class="text-center">{{ user.company.name }}</TableCell>
-            </TableRow>
-          </template>
-        </TableBody>
-      </Table>
+      <UserTable :filteredUsers="filteredUsers" />
     </main>
   </Suspense>
 </template>
-<style scoped>
-#table::-webkit-scrollbar {
-  display: none;
-}
-
-#table {
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
-</style>
